1c7fa5883f7b7559c3f4f956cfd27dde7a0e2fc2
Marcel Wiedenmann <marcel.wiedenmann@knime.com>
2021-08-02 17:03:30 +0200

    AP-17176: Adapt to changes in knime-python
    knime-deeplearning currently only works with the old Python kernel back
    end.
    
    AP-17176 (Deep Dive / Prototype: Get new backend running with old nodes)

--- org.knime.dl.python/src/org/knime/dl/python/core/DLPythonAbstractCommands.java
+++ org.knime.dl.python/src/org/knime/dl/python/core/DLPythonAbstractCommands.java
@@ -111,6 +111,7 @@ import org.knime.python2.extensions.serializationlibrary.interfaces.Type;
 import org.knime.python2.extensions.serializationlibrary.interfaces.impl.CellImpl;
 import org.knime.python2.extensions.serializationlibrary.interfaces.impl.RowImpl;
 import org.knime.python2.extensions.serializationlibrary.interfaces.impl.TableSpecImpl;
+import org.knime.python2.kernel.OldPythonKernelBackend;
 import org.knime.python2.kernel.PythonCommands;
 import org.knime.python2.kernel.PythonExecutionMonitor;
 import org.knime.python2.kernel.PythonIOException;
@@ -490,6 +491,8 @@ public abstract class DLPythonAbstractCommands implements DLPythonCommands {
         final PythonOutputListener stdOutListener = new PythonOutputLogger(stdErrLogger, stdOutWarningLogger, null);
 
         final PythonKernel kernel = context.getKernel();
+        final OldPythonKernelBackend kernelBackend = getLegacyKernelBackend(kernel);
+
         kernel.getDefaultStdoutListener().setDisabled(true);
         kernel.addStdoutListener(stdOutListener);
         kernel.addStderrorListener(stdErrListener);
@@ -520,7 +523,7 @@ public abstract class DLPythonAbstractCommands implements DLPythonCommands {
             // NB: We use our own DLTrainingTask to make sure the message handler is not unregistered.
             // Unregistering the handler can cause errors if requests for training data come in after the training is done.
             @SuppressWarnings("resource") // Closed by the kernel
-            final PythonCommands pythonCommands = kernel.getCommands();
+            final PythonCommands pythonCommands = kernelBackend.getCommands();
             @SuppressWarnings("resource") // Closed by the kernel
             final PythonMessaging pythonMessaging = pythonCommands.getMessaging();
             final DLTrainingTask trainingTask = new DLTrainingTask(pythonCommands.createExecuteCommand(b.toString()),
@@ -544,6 +547,16 @@ public abstract class DLPythonAbstractCommands implements DLPythonCommands {
             kernel.removeStderrorListener(stdOutListener);
             kernel.removeStderrorListener(stdErrListener);
         }
+    }
+
+    @SuppressWarnings("resource") // Kernel and back end are closed when the surrounding instance is closed.
+    private static OldPythonKernelBackend getLegacyKernelBackend(final PythonKernel kernel)
+        throws DLInvalidEnvironmentException {
+        if (!(kernel.getBackend() instanceof OldPythonKernelBackend)) {
+            throw new DLInvalidEnvironmentException("The KNIME Deep Learning integration currently only supports the " +
+                "legacy back end of the Python kernel. Please change your settings accordingly.");
+        }
+        return (OldPythonKernelBackend)kernel.getBackend();
     }
 
     /**
